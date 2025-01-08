b60e12cfdad6956ec6782cedbe5c481c2888029e
Bruno Vettori <bruno_vettori@proton.me>
2025-01-08 17:58:11 -0300

    forms

--- cmd/main.go
+++ cmd/main.go
@@ -22,8 +22,64 @@ func NewTemplates() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func NewContact(name string, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			NewContact("Bruno Vettori", "bruno_vettori@proton.me"),
+		},
+	}
+
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
 }
 
 func main() {
@@ -31,15 +87,28 @@ func main() {
 	e.Renderer = NewTemplates()
 	e.Use(middleware.Logger())
 
-	count := Count{Count: 0}
+	page := newPage()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(422, "form", formData)
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts, NewContact(name, email))
+		return c.Render(200, "display", page)
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
